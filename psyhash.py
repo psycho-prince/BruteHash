09ab8f22227560f78a74009c18ce7308aa476582
PSYCHO <65974917+psycho-prince@users.noreply.github.com>
2023-04-04 23:04:39 +0530

    Add files via upload
    Signed-off-by: PSYCHO <65974917+psycho-prince@users.noreply.github.com>

---
+++ psyhash.py
@@ -0,0 +1,57 @@
+import hashlib
+from art import *
+import os
+
+def clear():
+    # Clear screen for different operating systems
+    os.system('cls' if os.name=='nt' else 'clear')
+
+# Define the header
+def print_header():
+    clear()
+    tprint("PSYHASH--CRACKER")
+    print("Algorithms available: MD5 | SHA1 | SHA224 | SHA512 | SHA384")
+
+# Define the function to crack the hash
+def crack_hash(hash_type, wordlist_location, hashed):
+    word_list = open(wordlist_location, "r", encoding='utf-8', errors='ignore').read().splitlines()
+
+    for word in word_list:
+        if hash_type == "MD5":
+            hash_object = hashlib.md5(word.encode('utf-8'))
+            hashed_word = hash_object.hexdigest()
+
+        elif hash_type == "SHA1":
+            hash_object = hashlib.sha1(word.encode('utf-8'))
+            hashed_word = hash_object.hexdigest()
+
+        elif hash_type == "SHA224":
+            hash_object = hashlib.sha224(word.encode('utf-8'))
+            hashed_word = hash_object.hexdigest()
+
+        elif hash_type == "SHA512":
+            hash_object = hashlib.sha512(word.encode('utf-8'))
+            hashed_word = hash_object.hexdigest()
+
+        elif hash_type == "SHA384":
+            hash_object = hashlib.sha384(word.encode('utf-8'))
+            hashed_word = hash_object.hexdigest()
+
+        if hashed_word == hashed:
+            print("\033[1;34;47mHASH FOUND: {}\n".format(word))
+            return
+
+    print("\033[1;31;40mHASH NOT FOUND!\n")
+    return
+
+# Main function to run the program
+def main():
+    print_header()
+    hash_type = str(input("What's your HASH type? "))
+    wordlist_location = str(input("Enter wordlist location: "))
+    hashed = str(input("Enter HASH: "))
+    crack_hash(hash_type, wordlist_location, hashed)
+
+# Run the program
+if __name__ == "__main__":
+    main()
