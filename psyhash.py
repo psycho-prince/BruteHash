6e07100ba622bc733e9c7390e560e5a672319a92
PSYCHO <65974917+psycho-prince@users.noreply.github.com>
2023-04-04 23:26:15 +0530

    Update psyhash.py
    Signed-off-by: PSYCHO <65974917+psycho-prince@users.noreply.github.com>

--- psyhash.py
+++ psyhash.py
@@ -1,18 +1,12 @@
 import hashlib
-import subprocess
+import pyfiglet
+from termcolor import colored
 
-try:
-    import art
-except ImportError:
-    print("Art module not found. Installing...")
-    subprocess.check_call(['pip', 'install', 'art'])
-    import art
-
-ascii_banner = art.text2art('PSYCHO CRACKER')
+ascii_banner = pyfiglet.figlet_format('PSYCHO CRACKER')
 print(ascii_banner)
 
-print("HASHES CRACKER: ")
-art.tprint("PSYCHO")
+print("HASHES CRACKER: ", end=" ")
+print(colored('PSYCHO', 'red'))
 
 print("Algorithms available: MD5 | SHA1 | SHA224 | SHA512 | SHA384")
 
@@ -44,4 +38,4 @@ for word in word_list:
         hashed_word = hash_object.hexdigest()
     
     if hashed_word == hashed:
-        print("\033[1;34;47mHASH FOUND: {}\n".format(word))
+        print(colored("HASH FOUND: {}\n".format(word), 'green'))
